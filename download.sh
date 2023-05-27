c30192010f1542949d3582506805348be96ddd67
Wonjong Jang <wonjong@postech.ac.kr>
2023-05-27 12:51:37 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-URL=https://www.dropbox.com/s/ryc7v1ghsm54mr8/001000.pt?dl=0
+URL=https://drive.google.com/file/d/18UKYtQ46M0inNrgnBzfYub38-uzQHp7Y/view?usp=share_link
 mkdir -p ./checkpoint/StyleCariGAN
 OUT_FILE=./checkpoint/StyleCariGAN/001000.pt
 wget -N $URL -O $OUT_FILE
